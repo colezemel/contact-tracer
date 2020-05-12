29824887affb123cd313982c84e2987fe785cf79
Cole Zemel <cole.zemel@mail.utoronto.ca>
2020-05-12 14:41:59 -0400

    Use access_token generated from refresh_token

--- strava_api.js
+++ strava_api.js
@@ -1,9 +1,10 @@
 const auth_link = "https://www.strava.com/oauth/token";
 
 function getRoute(res) {
-  //   console.log(res);
-  const route_link = `https://www.strava.com/api/v3/athlete/activities?access_token=41f2dc45db840dc218b744f7f1be1559681aeb28`;
-  fetch(route_link).then(res => console.log(res.json()));
+  const route_link = `https://www.strava.com/api/v3/athlete/activities?access_token=${res.access_token}`;
+  fetch(route_link)
+    .then(res => res.json())
+    .then(res => console.log(res));
 }
 
 function reAuthorize() {
@@ -20,7 +21,9 @@ function reAuthorize() {
       refresh_token: "390eea5471370e9d896fe5b2ed15e75bcf8a8a27",
       grant_type: "refresh_token"
     })
-  }).then(res => getRoute(res));
+  })
+    .then(res => res.json())
+    .then(res => getRoute(res));
 }
 
 reAuthorize();
